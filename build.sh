983430a19a2f087b5298b005306496dae2e34454
Sagar <sagar@imageonline.co.in>
2026-05-11 19:29:02 +0530

    Fix build script and base-href for Vercel

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# Clone Flutter if it doesn't exist
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
-./flutter/bin/flutter build web --release
+
+# Add flutter to path
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Build the project properly for Vercel
+flutter build web --release --base-href "/"
